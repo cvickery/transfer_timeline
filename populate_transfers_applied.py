5e8b6214fc7bdbb994bab2f902707d07599bdb1a
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-03-08 09:41:21 -0500

    Update populate_transfers_applied.py
    Add NULL constraints; write anomalies to populate_report.csv

--- populate_transfers_applied.py
+++ populate_transfers_applied.py
@@ -30,32 +30,32 @@ drop table if exists transfers_applied cascade;
 
 create table transfers_applied (
 id serial primary key,
-student_id integer,
-src_institution text,
-transfer_model_nbr integer,
-enrollment_term date,
-enrollment_session text,
-articulation_term date,
-model_status text,
+student_id integer not NULL,
+src_institution text not NULL,
+transfer_model_nbr integer not NULL,
+enrollment_term date not NULL,
+enrollment_session text not NULL,
+articulation_term date not NULL,
+model_status text not NULL,
 posted_date date,
-src_subject text,
-src_catalog_nbr text,
-src_designation text,
-src_grade text,
-src_gpa real,
-src_course_id integer,
-src_offer_nbr integer,
-src_description text,
-academic_program text,
-units_taken real,
-dst_institution text,
-dst_designation text,
-dst_course_id integer,
-dst_offer_nbr integer,
-dst_subject text,
-dst_catalog_nbr text,
-dst_grade text,
-dst_gpa real
+src_subject text not NULL,
+src_catalog_nbr text not NULL,
+src_designation text not NULL,
+src_grade text not NULL,
+src_gpa real not NULL,
+src_course_id integer not NULL,
+src_offer_nbr integer not NULL,
+src_description text not NULL,
+academic_program text not NULL,
+units_taken real not NULL,
+dst_institution text not NULL,
+dst_designation text not NULL,
+dst_course_id integer not NULL,
+dst_offer_nbr integer not NULL,
+dst_subject text not NULL,
+dst_catalog_nbr text not NULL,
+dst_grade text not NULL,
+dst_gpa real not NULL
 );
 
 create unique index on transfers_applied (student_id,
@@ -66,50 +66,51 @@ create unique index on transfers_applied (student_id,
 """)
 conn.commit()
 
-m = 0
-n = len(open(latest, newline=None, errors='replace').readlines())
-with open(latest, newline=None, errors='replace') as csvfile:
-  reader = csv.reader(csvfile, )
-  for line in reader:
+with open('populate_report.csv', 'w') as report:
+  m = 0
+  n = len(open(latest, newline=None, errors='replace').readlines())
+  with open(latest, newline=None, errors='replace') as csvfile:
+    reader = csv.reader(csvfile, )
+    for line in reader:
 
-    if reader.line_num == 1:
-      headers = [h.lower().replace(' ', '_') for h in line]
-      print(headers)
-      placeholders = ((len(headers)) * '%s,').strip(', ')
-      cols = ', '.join([h for h in headers])
-      Row = namedtuple('Row', headers)
-    else:
-      m += 1
-      print(f'  {m:06,}/{n:06,}\r', end='', file=sys.stderr)
-      row = Row._make(line)
-
-      yr = 1900 + 100 * int(row.enrollment_term[0]) + int(row.enrollment_term[1:3])
-      mo = int(row.enrollment_term[-1])
-      da = 1
-      enrollment_term = datetime.date(yr, mo, da)
-
-      yr = 1900 + 100 * int(row.articulation_term[0]) + int(row.articulation_term[1:3])
-      mo = int(row.articulation_term[-1])
-      articulation_term = datetime.date(yr, mo, da)
-
-      if '/' in row.posted_date:
-        mo, da, yr = row.posted_date.split('/')
-        posted_date = datetime.date(int(yr), int(mo), int(da))
+      if reader.line_num == 1:
+        headers = [h.lower().replace(' ', '_') for h in line]
+        print(','.join(headers), file=report)
+        placeholders = ((len(headers)) * '%s,').strip(', ')
+        cols = ', '.join([h for h in headers])
+        Row = namedtuple('Row', headers)
       else:
-        posted_date = None
+        m += 1
+        print(f'  {m:06,}/{n:06,}\r', end='', file=sys.stderr)
+        row = Row._make(line)
 
-      value_tuple = (row.student_id, row.src_institution, row.transfer_model_nbr, enrollment_term,
-                     row.enrollment_session, articulation_term, row.model_status, posted_date,
-                     row.src_subject, row.src_catalog_nbr, row.src_designation, row.src_grade,
-                     row.src_gpa, row.src_course_id, row.src_offer_nbr, row.src_description,
-                     row.academic_program, row.units_taken, row.dst_institution,
-                     row.dst_designation, row.dst_course_id, row.dst_offer_nbr, row.dst_subject,
-                     row.dst_catalog_nbr, row.dst_grade, row.dst_gpa)
-      cursor.execute(f'insert into transfers_applied ({cols}) values ({placeholders}) '
-                     f'on conflict do nothing',
-                     value_tuple)
-      if cursor.rowcount == 0:
-        print(line)
+        yr = 1900 + 100 * int(row.enrollment_term[0]) + int(row.enrollment_term[1:3])
+        mo = int(row.enrollment_term[-1])
+        da = 1
+        enrollment_term = datetime.date(yr, mo, da)
 
-conn.commit()
-exit()
+        yr = 1900 + 100 * int(row.articulation_term[0]) + int(row.articulation_term[1:3])
+        mo = int(row.articulation_term[-1])
+        articulation_term = datetime.date(yr, mo, da)
+
+        if '/' in row.posted_date:
+          mo, da, yr = row.posted_date.split('/')
+          posted_date = datetime.date(int(yr), int(mo), int(da))
+        else:
+          posted_date = None
+
+        value_tuple = (row.student_id, row.src_institution, row.transfer_model_nbr, enrollment_term,
+                       row.enrollment_session, articulation_term, row.model_status, posted_date,
+                       row.src_subject, row.src_catalog_nbr, row.src_designation, row.src_grade,
+                       row.src_gpa, row.src_course_id, row.src_offer_nbr, row.src_description,
+                       row.academic_program, row.units_taken, row.dst_institution,
+                       row.dst_designation, row.dst_course_id, row.dst_offer_nbr, row.dst_subject,
+                       row.dst_catalog_nbr, row.dst_grade, row.dst_gpa)
+        cursor.execute(f'insert into transfers_applied ({cols}) values ({placeholders}) '
+                       f'on conflict do nothing',
+                       value_tuple)
+        if cursor.rowcount == 0:
+          print(','.join(line), file=report)
+
+  conn.commit()
+  exit()
