26b3a361e37a40a14709f49ee62741e9b685bdf2
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-03-10 10:27:06 -0500

    Update populate_transfers_applied.py
    Use separate db (cuny_transfers) so history won't get wiped on
    when cuny_curriculum is rebuilt.

--- populate_transfers_applied.py
+++ populate_transfers_applied.py
@@ -11,106 +11,138 @@ from psycopg2 import errors
 possibles = Path('/Users/vickery/transfers_applied/downloads').glob('*FULL*')
 latest = None
 for possible in possibles:
-  if latest is None or possible.stat().m_time > latest.stat.m_time:
+  if latest is None or possible.stat().st_mtime > latest.stat().st_mtime:
     latest = possible
 if latest is None:
   sys.exit('No population source found.')
 
-print('You are about to overwrite years of intense effort. Repent or Proceed (r/P)? ',
+print('You are about to overwrite years of intense effort. Repent or Proceed? (r/P) ',
       end='', file=sys.stderr)
 if input().startswith('r'):
   print('Ill-advised consequences averted.', file=sys.stderr)
   exit()
 
-conn = PgConnection()
+repeatable = dict()
+
+conn = PgConnection('cuny_transfers')
 cursor = conn.cursor()
 
 cursor.execute("""
 drop table if exists transfers_applied cascade;
 
 create table transfers_applied (
-id serial primary key,
-student_id integer not NULL,
-src_institution text not NULL,
-transfer_model_nbr integer not NULL,
-enrollment_term date not NULL,
-enrollment_session text not NULL,
-articulation_term date not NULL,
-model_status text not NULL,
-posted_date date,
-src_subject text not NULL,
-src_catalog_nbr text not NULL,
-src_designation text not NULL,
-src_grade text not NULL,
-src_gpa real not NULL,
-src_course_id integer not NULL,
-src_offer_nbr integer not NULL,
-src_description text not NULL,
-academic_program text not NULL,
-units_taken real not NULL,
-dst_institution text not NULL,
-dst_designation text not NULL,
-dst_course_id integer not NULL,
-dst_offer_nbr integer not NULL,
-dst_subject text not NULL,
-dst_catalog_nbr text not NULL,
-dst_grade text not NULL,
-dst_gpa real not NULL
+  id serial primary key,
+  student_id integer not NULL,
+  src_institution text not NULL,
+  transfer_model_nbr integer not NULL,
+  enrollment_term date not NULL,
+  enrollment_session text not NULL,
+  articulation_term date not NULL,
+  model_status text not NULL,
+  posted_date date,
+  src_subject text not NULL,
+  src_catalog_nbr text not NULL,
+  src_designation text not NULL,
+  src_grade text not NULL,
+  src_gpa real not NULL,
+  src_course_id integer not NULL,
+  src_offer_nbr integer not NULL,
+  src_repeatable boolean, -- See ./lookup_failures for courses not found
+  src_description text not NULL,
+  academic_program text not NULL,
+  units_taken real not NULL,
+  dst_institution text not NULL,
+  dst_designation text not NULL,
+  dst_course_id integer not NULL,
+  dst_offer_nbr integer not NULL,
+  dst_subject text not NULL,
+  dst_catalog_nbr text not NULL,
+  dst_grade text not NULL,
+  dst_gpa real not NULL
 );
 
-create unique index on transfers_applied (student_id,
-                                          src_course_id,
-                                          src_offer_nbr,
-                                          dst_institution,
-                                          posted_date);
+create index on transfers_applied (student_id,
+                                   src_course_id,
+                                   src_offer_nbr,
+                                   dst_institution,
+                                   posted_date);
 """)
 conn.commit()
 
-with open('populate_report.csv', 'w') as report:
-  m = 0
-  n = len(open(latest, newline=None, errors='replace').readlines())
-  with open(latest, newline=None, errors='replace') as csvfile:
-    reader = csv.reader(csvfile, )
-    for line in reader:
+with open('./lookup_failures', 'w') as lookup_failures:
+  with open('populate_report.csv', 'w') as report:
+    m = 0
+    n = len(open(latest, newline=None, errors='replace').readlines())
+    with open(latest, newline=None, errors='replace') as csvfile:
+      reader = csv.reader(csvfile, )
+      for line in reader:
 
-      if reader.line_num == 1:
-        headers = [h.lower().replace(' ', '_') for h in line]
-        print(','.join(headers), file=report)
-        placeholders = ((len(headers)) * '%s,').strip(', ')
-        cols = ', '.join([h for h in headers])
-        Row = namedtuple('Row', headers)
-      else:
-        m += 1
-        print(f'  {m:06,}/{n:06,}\r', end='', file=sys.stderr)
-        row = Row._make(line)
-
-        yr = 1900 + 100 * int(row.enrollment_term[0]) + int(row.enrollment_term[1:3])
-        mo = int(row.enrollment_term[-1])
-        da = 1
-        enrollment_term = datetime.date(yr, mo, da)
-
-        yr = 1900 + 100 * int(row.articulation_term[0]) + int(row.articulation_term[1:3])
-        mo = int(row.articulation_term[-1])
-        articulation_term = datetime.date(yr, mo, da)
-
-        if '/' in row.posted_date:
-          mo, da, yr = row.posted_date.split('/')
-          posted_date = datetime.date(int(yr), int(mo), int(da))
+        if reader.line_num == 1:
+          headers = [h.lower().replace(' ', '_') for h in line]
+          # print(','.join(headers), file=report)
+          cols = [h for h in headers]
+          cols.insert(1 + cols.index('src_offer_nbr'), 'src_repeatable')
+          # print('cols array', len(cols), cols)
+          placeholders = ((len(cols)) * '%s,').strip(', ')
+          # print('placeholders', placeholders.count('s'), placeholders)
+          cols = ', '.join([c for c in cols])
+          # print('cols string', cols.count(',') + 1, cols)
+          Row = namedtuple('Row', headers)
         else:
-          posted_date = None
+          m += 1
+          print(f'  {m:06,}/{n:06,}\r', end='', file=sys.stderr)
+          row = Row._make(line)
 
-        value_tuple = (row.student_id, row.src_institution, row.transfer_model_nbr, enrollment_term,
-                       row.enrollment_session, articulation_term, row.model_status, posted_date,
-                       row.src_subject, row.src_catalog_nbr, row.src_designation, row.src_grade,
-                       row.src_gpa, row.src_course_id, row.src_offer_nbr, row.src_description,
-                       row.academic_program, row.units_taken, row.dst_institution,
-                       row.dst_designation, row.dst_course_id, row.dst_offer_nbr, row.dst_subject,
-                       row.dst_catalog_nbr, row.dst_grade, row.dst_gpa)
-        cursor.execute(f'insert into transfers_applied ({cols}) values ({placeholders}) '
-                       f'on conflict do nothing',
-                       value_tuple)
-        if cursor.rowcount == 0:
-          print(','.join(line), file=report)
+          yr = 1900 + 100 * int(row.enrollment_term[0]) + int(row.enrollment_term[1:3])
+          mo = int(row.enrollment_term[-1])
+          da = 1
+          enrollment_term = datetime.date(yr, mo, da)
+
+          yr = 1900 + 100 * int(row.articulation_term[0]) + int(row.articulation_term[1:3])
+          mo = int(row.articulation_term[-1])
+          articulation_term = datetime.date(yr, mo, da)
+
+          if '/' in row.posted_date:
+            mo, da, yr = row.posted_date.split('/')
+            posted_date = datetime.date(int(yr), int(mo), int(da))
+          else:
+            posted_date = None
+
+          src_course_id = int(row.src_course_id)
+          src_offer_nbr = int(row.src_offer_nbr)
+          src_catalog_nbr = row.src_catalog_nbr.strip()
+          dst_course_id = int(row.dst_course_id)
+          dst_offer_nbr = int(row.dst_offer_nbr)
+          dst_catalog_nbr = row.dst_catalog_nbr.strip()
+
+          try:
+            src_repeatable = repeatable[(int(row.src_course_id), (row.src_offer_nbr))]
+          except KeyError:
+            cursor.execute(f'select repeatable from cuny_courses where course_id = '
+                           f'{src_course_id} and offer_nbr = {src_offer_nbr}')
+            if cursor.rowcount != 1:
+              print(f'Repeatable lookup for {src_course_id:06}.{src_offer_nbr}'
+                    f' returned {cursor.rowcount} rows: {row.src_institution} '
+                    f'{row.src_subject}-{src_catalog_nbr}', file=lookup_failures)
+              src_repeatable = None
+            else:
+              src_repeatable = cursor.fetchone().repeatable
+            repeatable[(int(row.src_course_id), (row.src_offer_nbr))] = src_repeatable
+
+          value_tuple = (row.student_id, row.src_institution, row.transfer_model_nbr,
+                         enrollment_term, row.enrollment_session, articulation_term,
+                         row.model_status, posted_date, row.src_subject, src_catalog_nbr,
+                         row.src_designation, row.src_grade, row.src_gpa, row.src_course_id,
+                         row.src_offer_nbr, src_repeatable, row.src_description,
+                         row.academic_program, row.units_taken, row.dst_institution,
+                         row.dst_designation, row.dst_course_id, row.dst_offer_nbr, row.dst_subject,
+                         dst_catalog_nbr, row.dst_grade, row.dst_gpa)
+          # print('values tuple', len(value_tuple), value_tuple)
+          cursor.execute(f'insert into transfers_applied ({cols}) values ({placeholders}) '
+                         f'on conflict do nothing',
+                         value_tuple)
+          if cursor.rowcount == 0:
+            print(','.join(line), file=report)
 
   conn.commit()
   exit()
