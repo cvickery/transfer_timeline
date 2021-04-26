4d5362dccd1664be60c03e2346ae4ee3d91b7190
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-04-26 11:14:16 -0400

    Add link_dbs.sql
    How to link two databases.

---
+++ link_dbs.sql
@@ -0,0 +1,9 @@
+-- Link all tables in cuny_curriculum to cuny_transfers.
+-- This should actually be done using two schemas for one db, but I didn't do that, so this is a
+-- temporary hack.
+-- The wrapper persists, so this is more documentatary than procedural.
+
+create extension postgres_fdw;
+create server cunycu foreign data wrapper postgres_fdw options (dbname 'cuny_curriculum');
+create user mapping for vickery server cunycu;
+import foreign schema public from server cunycu into public;
