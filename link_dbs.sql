a165d34031ce5a7fe0644fc965895877a67b8a47
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-06-30 12:31:51 -0400

    Update link_dbs.sql
    Document the documentary feature of the code.

--- link_dbs.sql
+++ link_dbs.sql
@@ -2,6 +2,8 @@
 -- This should actually be done using two schemas for one db, but I didn't do that, so this is a
 -- temporary hack.
 -- The wrapper persists, so this is more documentatary than procedural.
+-- Actually, the wrapper was a bother, and I removed it. But this is sql is still of documentarty
+-- value. Maybe.
 
 create extension postgres_fdw;
 create server cunycu foreign data wrapper postgres_fdw options (dbname 'cuny_curriculum');
