2054b326b13fef84864ff3ab6d43209bf5153332
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2023-03-10 16:57:18 -0500

    Fix timeline_events.py

--- timeline_events.py
+++ timeline_events.py
@@ -42,68 +42,67 @@ for line in reader:
 
 with psycopg.connect('dbname=cuny_transfers') as conn:
   with conn.cursor(row_factory=dict_row) as cursor:
+    all_student_ids = []
     for institution, student_ids in sorted(cohorts.items()):
       print(institution, len(student_ids))
-      student_id_list = ','.join([f'{student_id}' for student_id in student_ids])
+      all_student_ids += student_ids
+    student_id_list = ','.join([f'{student_id}' for student_id in all_student_ids])
 
-      # Admission Events
-      output_file = open(f'cohort_reports/{cohort_code}_admissions_'
-                         f'{str(datetime.today())[0:10]}.csv', 'w')
-      writer = csv.writer(output_file)
-      cursor.execute(f"""
-      select * from admissions
-       where institution = %s
-         and student_id in ({student_id_list})
-    order by (institution, student_id, action_date)
-      """, (institution,))
-      header_row = None
-      for row in cursor:
-        if header_row is None:
-          header_row = row.keys()
-          writer.writerow(header_row)
-        else:
-          out_row = [f'{value}' for value in row.values()]
-          writer.writerow(out_row)
-      output_file.close()
+    # Admission Events
+    output_file = open(f'cohort_reports/{cohort_code}_admissions_'
+                       f'{str(datetime.today())[0:10]}.csv', 'w')
+    writer = csv.writer(output_file)
+    cursor.execute(f"""
+    select * from admissions
+     where student_id in ({student_id_list})
+     order by (institution, student_id, action_date)
+    """)
+    header_row = None
+    for row in cursor:
+      if header_row is None:
+        header_row = row.keys()
+        writer.writerow(header_row)
+      else:
+        out_row = [f'{value}' for value in row.values()]
+        writer.writerow(out_row)
+    output_file.close()
 
-      # Registration Events
-      output_file = open(f'cohort_reports/{cohort_code}_registrations_'
-                         f'{str(datetime.today())[0:10]}.csv', 'w')
-      writer = csv.writer(output_file)
-      cursor.execute(f"""
-      select * from registrations
-       where institution = %s
-         and student_id in ({student_id_list})
-    order by (institution, student_id, add_date, drop_date)
-      """, (institution,))
-      header_row = None
-      for row in cursor:
-        if header_row is None:
-          header_row = row.keys()
-          writer.writerow(header_row)
-        else:
-          out_row = [f'{value}' for value in row.values()]
-          writer.writerow(out_row)
-      output_file.close()
+    # Registration Events
+    output_file = open(f'cohort_reports/{cohort_code}_registrations_'
+                       f'{str(datetime.today())[0:10]}.csv', 'w')
+    writer = csv.writer(output_file)
+    cursor.execute(f"""
+    select * from registrations
+     where student_id in ({student_id_list})
+     order by (institution, student_id, add_date, drop_date)
+    """)
+    header_row = None
+    for row in cursor:
+      if header_row is None:
+        header_row = row.keys()
+        writer.writerow(header_row)
+      else:
+        out_row = [f'{value}' for value in row.values()]
+        writer.writerow(out_row)
+    output_file.close()
 
-      # Evaluation Events (note dst_institution field name; not just institution)
-      output_file = open(f'cohort_reports/{cohort_code}_evaluations_'
-                         f'{str(datetime.today())[0:10]}.csv', 'w')
-      writer = csv.writer(output_file)
-      cursor.execute(f"""
-      select * from transfers_applied
-       where dst_institution = %s
-         and student_id in ({student_id_list})
-    order by (dst_institution, student_id, posted_date)
-      """, (institution,))
-      header_row = None
-      for row in cursor:
-        if header_row is None:
-          header_row = row.keys()
-          writer.writerow(header_row)
-        else:
-          out_row = [f'{value}' for value in row.values()]
-          writer.writerow(out_row)
-      output_file.close()
+    # Evaluation Events (note dst_institution field name; not just institution)
+    output_file = open(f'cohort_reports/{cohort_code}_evaluations_'
+                       f'{str(datetime.today())[0:10]}.csv', 'w')
+    writer = csv.writer(output_file)
+    cursor.execute(f"""
+    select * from transfers_applied
+     where student_id in ({student_id_list})
+     order by (dst_institution, student_id, posted_date)
+    """)
+    header_row = None
+    for row in cursor:
+      if header_row is None:
+        header_row = row.keys()
+        writer.writerow(header_row)
+      else:
+        out_row = [f'{value}' for value in row.values()]
+        writer.writerow(out_row)
+    output_file.close()
 
 print(f'{(datetime.now() - start_at).seconds} seconds')
